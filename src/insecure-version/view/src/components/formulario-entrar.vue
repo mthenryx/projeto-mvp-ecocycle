5c5ce8cb93b411cc730c2dc07c40c7d10e2b404c
Matheus Henry <09matheushenry@gmail.com>
2026-08-13 21:03:52 -0300

    feat: organizando estrutura da página, vou usar Vue.js, pois é um framework que decidi aprender então preciso praticar!

---
+++ src/insecure-version/view/src/components/formulario-entrar.vue
@@ -0,0 +1,82 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="login">
+        <div class="login-header">
+            <div class="alterar"></div>
+            <span>Login</span>
+            <span>Cadastro</span>
+        </div>
+        <form>
+            <input 
+                type="email" 
+                placeholder="Ex: ecocycle@gmail.com" 
+                v-model="email" 
+                class="input"
+            />
+            <input 
+                type="text" 
+                placeholder="*******" 
+                v-model="password" 
+                class="input"
+            />
+            <button type="submit" @click="fazerLogin" class="btn-entrar">
+                Entrar
+            </button>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+    .login {
+        border: 1px solid #000;
+        width: 30%;
+        height: 40%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        border-radius: 10px;
+        gap: 30px;
+        font-family: 'Inter', sans-serif;
+    }
+
+    .login-header {
+
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+
+    .input {
+        height: 35px;
+        width: 100%;
+        background-color: #D9D9D9;
+        border: none;
+        border-radius: 10px;
+    }
+
+    .btn-entrar {
+        height: 35px;
+        width: 100%;
+        background-color: #2D8204;
+        border: none;
+        border-radius: 10px;
+        color: white;
+        transition: 0.3s ease;
+    }
+
+    .btn-entrar:hover {
+        background-color: #1E5A02;
+        cursor: pointer;
+    }
+
+    .btn-entrar:active {
+        transform: scale(0.95);
+    }
+</style>
---
+++ src/insecure-version/view/src/views/login.vue
@@ -0,0 +1,64 @@
+<script setup>
+    import FormularioEntrar from '../components/formulario-entrar.vue'
+</script>
+
+<template>
+    <div class="pagina-login">
+        <img 
+            src="../assets/imgs/coluna-verde.png" 
+            alt="Coluna Verde" 
+            class="coluna-verde-top" 
+        />
+        <img 
+            src="../assets/imgs/logo-marca.png" 
+            alt="Logo Ecocycle" 
+            class="logo-marca"
+        />
+        <FormularioEntrar />
+        <img 
+            src="../assets/imgs/coluna-verde.png" 
+            alt="Coluna Verde" 
+            class="coluna-verde-bottom" 
+        />
+    </div>
+</template>
+
+<style scoped>
+    :global(html, body) {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+        overflow: hidden;
+    }
+
+    :global(#app) {
+        height: 100%;
+    }
+
+    .pagina-login {
+        width: 100%;
+        height: 100vh;
+        overflow: hidden;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .coluna-verde-top {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 40%;
+        height: auto;
+        transform: rotate(180deg);
+    }
+
+    .coluna-verde-bottom {
+        position: absolute;
+        bottom: 0px;
+        right: 0;
+        width: 40%;
+        height: auto;
+    }
+</style>
